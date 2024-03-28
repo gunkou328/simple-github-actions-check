675d0fe707da4f378e117b72dd60cc24f255aaad
gunji kosuke <gunji@328w.co.jp>
2024-03-28 22:33:57 +0900

    vitestを失敗させる

--- tests/scripts/index.test.ts
+++ tests/scripts/index.test.ts
@@ -2,6 +2,6 @@ import { expect, test } from 'vitest'
 import { sum } from '../../src/scripts/sum'
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-  // expect(sum(1, 3)).toBe(3)
+  // expect(sum(1, 2)).toBe(3)
+  expect(sum(1, 3)).toBe(3)
 })
