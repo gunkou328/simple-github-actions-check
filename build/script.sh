c85651af4b2c8e221aafacba2b5bcbac357faa34
gunji kosuke <gunji@328w.co.jp>
2024-11-08 18:56:03 +0900

    差分

--- build/script.sh
+++ build/script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -euo pipefail
 
 # 第一引数は最新ファイル、第二引数は古いファイル
 # 例）
@@ -17,7 +16,7 @@ diff -qr "$1" "$2" -x .DS_Store |
             site_root_path_name=$(echo "$full_path" | sed -e "s|$1||; s|$2||")
 
             if [[ "$path" == "$1"* ]]; then
-                # rsync -aR "$full_path" ./release
+                rsync -aR "$full_path" ./release
                 echo "新規: $site_root_path_name"
             elif [[ "$path" == "$2"* ]]; then
                 echo "削除: $site_root_path_name"
@@ -25,7 +24,9 @@ diff -qr "$1" "$2" -x .DS_Store |
         else
             full_path=$(echo "$line" | awk '{print $2}')
             site_root_path_name=$(echo "$full_path" | sed -e "s|$1||; s|$2||")
-            # rsync -aR "$full_path" ./release
+            rsync -aR "$full_path" ./release
             echo "差分: $site_root_path_name"
         fi
     done
+|
+LC_COLLATE=C sort -r
