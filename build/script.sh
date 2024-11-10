f55075c0f99cf0c328c099d37e10d4c7ed294d68
gunji kosuke <gunji@328w.co.jp>
2024-11-10 22:35:49 +0900

    差分

--- build/script.sh
+++ build/script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# set -euo pipefail
+set -euo pipefail
 
 # 第一引数は最新ファイル、第二引数は古いファイル
-# 例）
-# `bash script.sh ./new ./old > diff.txt`
+# 例） `bash script.sh ./new ./old > diff.txt`
 # を実行することで差分ファイルの抽出と差分ファイル一覧テキストが出力されます。
 
-echo "最新ファイル: $1"
-echo "古いファイル: $2"
-
-diff -qr "$1" "$2" -x .DS_Store |
+# diffの戻り値が1でビルドジョブが止まるので、一時的にpipefailを無効化
+{
+    set +o pipefail
+    diff -qr "$1" "$2" -x .DS_Store || true
+} |
 (
     while IFS= read -r line; do
         status=$(echo "$line" | awk '{print $1}')
@@ -21,7 +21,7 @@ diff -qr "$1" "$2" -x .DS_Store |
             site_root_path_name=$(echo "$full_path" | sed -e "s|$1||; s|$2||")
 
             if [[ "$path" == "$1"* ]]; then
-                rsync -aR "$full_path" ./release || { echo "rsync failed for $full_path"; exit 1; }
+                rsync -aR "$full_path" ./release
                 echo "新規: $site_root_path_name"
             elif [[ "$path" == "$2"* ]]; then
                 echo "削除: $site_root_path_name"
@@ -29,7 +29,7 @@ diff -qr "$1" "$2" -x .DS_Store |
         else
             full_path=$(echo "$line" | awk '{print $2}')
             site_root_path_name=$(echo "$full_path" | sed -e "s|$1||; s|$2||")
-            rsync -aR "$full_path" ./release || { echo "rsync failed for $full_path"; exit 1; }
+            rsync -aR "$full_path" ./release
             echo "差分: $site_root_path_name"
         fi
     done
